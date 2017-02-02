369b3b9c8c8adb0db52208821b1ba159505f7360
Thorre <thorre2000@gmail.com>
2017-02-02 21:11:17 +0100

    Fixed merge!

--- bottom-bar/src/main/java/com/roughike/bottombar/BottomBarTab.java
+++ bottom-bar/src/main/java/com/roughike/bottombar/BottomBarTab.java
@@ -7,6 +7,7 @@ import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Typeface;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.annotation.ColorInt;
 import android.support.annotation.VisibleForTesting;
